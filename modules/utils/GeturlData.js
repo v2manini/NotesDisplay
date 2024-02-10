abf5cb96753845770a7ed99d557ff4a57f325089
RicherBel <Yourmother@alfa>
2024-02-10 16:29:27 -0300

    Se modularizo eel geturldata y se le agregó opciones al meta-scraper

--- modules/utils/GeturlData.js
+++ modules/utils/GeturlData.js
@@ -1,43 +1,42 @@
 const getMetaData = require('metadata-scraper');
 
 async function geturldata(urls) { 
+    try {  
+        let ArraAux = [];
+        return ArraAux = getDataFromUrl(urls);
+    } catch (error) {console.log("Error del geturldata ",error)};
+};
+
+async function getDataFromUrl(urls) {  // para futuro
     let ArraAux = [];
     let Urldata ;
     let idNum = 0;
-try {  
+    let objMetaScap;
+
     for (let i = 0; i < urls.length; i++) {
         if (!urls[i].startsWith("http")) return;
-         
+
+        objMetaScap = {
+            url: urls[i],
+            forceImageHttps: false, 
+        };
+    
         if (urls[i].endsWith("jpg")|| urls[i].endsWith("jpeg") || urls[i].endsWith("png") || urls[i].endsWith("webp") || urls[i].endsWith("gif")) {
-            Urldata = await getMetaData(urls[i]);
+            Urldata = await getMetaData(objMetaScap);
             ArraAux.push(getObje(idNum,urls[i],"Imagen/Pintura","",Urldata.provider,"","Ninguna",urls[i])); // Urldata.provider usar el urls[i]
             idNum++;
         } else {
-            Urldata = await getMetaData(urls[i]);
+            Urldata = await getMetaData(objMetaScap);
             //console.log(Urldata); 
             if (!Urldata.description) Urldata.description = "Ninguna"; 
            ArraAux.push(getObje(idNum,Urldata.url,Urldata.title,Urldata.description.slice(0,500),Urldata.provider,"","Ninguna",Urldata.image));
            idNum++;
         }
-        console.log("Se completado el url num ",i)
-    }
+        console.log("Se completado el url num ",i+1)
+    };
+    console.log("\n");
     return ArraAux;
-} catch (error) {console.log("Error del geturldata ",error)}
-}
-
-// async function getSubdata(urls,idNum) {  // para futuro
-//     let = objx;
-//     if (urls.endsWith("jpg")|| urls.endsWith("jpeg") || urls.endsWith("png") || urls.endsWith("webp") || urls.endsWith("gif")) {
-//         Urldata = await getMetaData(urls[i]);
-//         objx = getObje(idNum,urls,"Imagen/Pintura","",Urldata.provider,"","Ninguna",urls); // Urldata.provider usar el urls[i]
-//     } else {
-//         Urldata = await getMetaData(urls);
-//         //console.log(Urldata);           
-//         objx = getObje(idNum,Urldata.url,Urldata.title,Urldata.description.slice(0,500),Urldata.provider,"","Ninguna",Urldata.image);
-//     }
-//     idNum++;
-//     return objx;
-// }
+};
 
 function getObje(idNum,url,title,description,provider,Subtipo,limag,pimag) {
     Objs =  {
