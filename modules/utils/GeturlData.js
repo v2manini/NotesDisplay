062beaed90747595ad9f017f660a42548440d843
RicherBel <Yourmother@alfa>
2024-05-08 17:21:48 -0300

    Se ha modularizado algunos apartados (Falta poner la api de Youtube)

--- modules/utils/GeturlData.js
+++ modules/utils/GeturlData.js
@@ -10,7 +10,6 @@ async function geturldata(urls) {
 async function getDataFromUrl(urls) {  // para futuro
     let ArraAux = [];
     let Urldata ;
-    let idNum = 0;
     let objMetaScap;
 
     for (let i = 0; i < urls.length; i++) {
@@ -20,29 +19,30 @@ async function getDataFromUrl(urls) {  // para futuro
             url: urls[i],
             forceImageHttps: false, 
         };
-    
+        // Si el url es una imagen
         if (urls[i].endsWith("jpg")|| urls[i].endsWith("jpeg") || urls[i].endsWith("png") || urls[i].endsWith("webp") || urls[i].endsWith("gif")) {
             Urldata = await getMetaData(objMetaScap);
             ArraAux.push(getObje(idNum,urls[i],"Imagen/Pintura","",Urldata.provider,"","Ninguna",urls[i])); // Urldata.provider usar el urls[i]
             idNum++;
-        } else {
+        // }if (urls[i].startsWith("https://youtu.be")|| urls[i].startsWith("https://www.youtube")) { // Si el url es de YT
+        //     console.log("Video de YT ");
+
+        }else {
             Urldata = await getMetaData(objMetaScap);
-            //console.log(Urldata); 
             if (!Urldata.description) Urldata.description = "Ninguna"; 
-           ArraAux.push(getObje(idNum,Urldata.url,Urldata.title,Urldata.description.slice(0,500),Urldata.provider,"","Ninguna",Urldata.image));
-           idNum++;
-        }
+        
+           ArraAux.push(getObje(Urldata.url,Urldata.title,Urldata.description.slice(0,500),Urldata.provider,"","Ninguna",Urldata.image));
+        };
         console.log("Se completado el url num ",i+1)
     };
     console.log("\n");
     return ArraAux;
 };
 
-function getObje(idNum,url,title,description,provider,Subtipo,limag,pimag) {
+function getObje(url,title,description,provider,Subtipo,limag,pimag) {
     Objs =  {
-        id : idNum,
         url : url,
-        nombre : title,
+        nombre : title || url,
         descrip : description,
         tipo : provider,
         Subtipo: Subtipo,
--- public/js/tableRender.js
+++ public/js/tableRender.js
@@ -1,38 +1,3 @@
-const optionsDefaul = "Datos para elegir";
-var DataJsonGloabla;
-
-window.addEventListener('load', function(){
-    let selec = this.document.getElementById("Idselec");
-    if (selec) {
-            ajaxPost("/getjson", null, function(data){
-                data = JSON.parse(data);
-                selecfill(data,selec);      
-            });
-    }
-});
-
-function selecAct() { //Selecciona el json que se quiera leer
-    let selec = document.getElementById("Idselec").value;
-    if(selec == optionsDefaul) return;
-
-    ajaxPost("/getdata", {selec}, function(data){ 
-        data = JSON.parse(data);
-        DataJsonGloabla = data;
-        tablefill(data,selec);      
-    });
-
-    console.log(document.getElementsByClassName("dataconteiner"))
-
-}
-
-function selecfill(data,selec) { // Muestra todos los archivos json en el selector
-    let fill = `<option selected>${optionsDefaul}</option>`;
-    for (let i = 0; i < data.length; i++) {
-        fill += `<option value ="${data[i]}">${data[i]}</option>`;
-    }
-    selec.innerHTML = fill;
-}
-
 function tablefill(data,jsonfilename) { // Crea la tabla con los datos 
     let table = "";
     let jsonUrl = "";
